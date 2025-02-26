04b6fc2f9bcce9b3a11d361f69b541b5369d032d
Nate06b <500210725@fvtc.edu>
2025-02-26 15:27:02 -0600

    Added File input/Output

--- NMB.MathQuiz/Main.cpp
+++ NMB.MathQuiz/Main.cpp
@@ -6,14 +6,21 @@
 #include <vector>
 #include <conio.h>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+	// creating filePath and line variables to use for input and output 
+	string filePath = "QuizResults.txt";
+	string line;	
+
+
 	
+
 	cout << "Welcome to Math Quiz!";
-	
+
 	vector<string> questions = {
 		"1 + 1 =",
 		"4 / 2 =",
@@ -28,7 +35,7 @@ int main()
 	};
 	vector<int> answers = { 2, 2, 15, 3, 10, 2, 20, 16, 0, 1 };
 	vector<string> results;
-		
+
 
 	for (int i = 0; i < 3; i++)
 	{ // Ask 3 questions for randomization
@@ -52,11 +59,41 @@ int main()
 
 		results.push_back(question + " " + to_string(answer) + ": ");
 	}
-	cout << "\nResults: ";
-	for (const string& result : results)
+	
+
+	// Save results to file
+	ofstream ofs(filePath);
+	if (ofs)
 	{
-		cout << result;
+for (const string& result : results)
+	{
+		ofs << result << "\n";
 	}
+		ofs.close();
+		cout << "Results saved to " << filePath << "\n";
+	}
+	else
+	{
+		cout << "Error opening file for writing.\n";
+		return 1;
+	}
+	// reaad results for file
+	ifstream ifs(filePath);
+	if (ifs)
+	{
+		cout << "Reading results from " << filePath << ":\n";
+		while (getline(ifs, line))
+		{
+			cout << line << "\n";
+		}
+		ifs.close();
+	}
+	else
+	{
+		cout << "Error opening file for reading.\n";
+		return 1;
+	}
+
 
 	(void)_getch();
 	return 0;
